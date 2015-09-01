4680ef56c480f07087327f39c604cea406e34fae
Jack <jack.rc.king@gmail.com>
2015-09-01 15:43:20 +0100

    uh?

--- calcp.f
+++
@@ -1,9 +0,0 @@
-	subroutine calcp
-	include "commonblock"
-c	use an equation of state to find the pressure
-	do i=1,n+ng
-c		Tamman EoS, but ideal gas if pc=0,gm=1.4
-		p(i) = (gm(i)-1.0)*u(i)*rho(i) - gm(i)*pc(i)
-	end do
-	return
-	end
