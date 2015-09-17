8467954cb706ce72892b7d4d7415925b47a64629
Jack <jack.rc.king@gmail.com>
2015-09-17 18:51:03 +0100

    isometric grid implemented

--- README.md
+++ README.md
@@ -15,13 +15,14 @@ Features:
   * Box shaped domain;
   * 'Wall' boundary conditions;
   * Some Octave scripts to interpolate results to grid and plot.
+  * Isometric grid of starting particles (for 2d Sedov)
 
 Bugs and so on:
   * Why does \Omega\ne 1.0 cause the wrong results?;
   * 1d Sod - requires smoothing of initial conditions;
   * Create some inflow/outflow boundary conditions;
-  * Should have isometric arrangement to start with;
   * Implement w.
+  * Isometric grid doesn't prevent "grid" like errors appearing. Something to do with particles being allowed to coallesce? Artificial viscosity fix somehow?
 
 ![alt text](https://raw.githubusercontent.com/jrcking/kingsph/master/img/tron.jpg  "Sedov explosion particle positions")
 
--- src/setupsedov2d.f
+++ src/setupsedov2d.f
@@ -1,29 +1,29 @@
 	subroutine setupsedov2d
 	include "commonblock"
-	integer crn
+	integer nx,ny
 	real dx,dy,dz,x,y,z,radius
 c	set counters and number particles (to go in setup/param 
 c	file in due course).
 	dims=2
 	eta = 0.4
 	ntmax = 100
-	crn=49  !sqr root of the TOTAL number, and best ODD
-	n = crn**dims
-c
 	do l=1,3
 	   rlow(l)=-1.0d0
 	   rhigh(l)=1.0d0
 	end do
-c
-	dx = (rhigh(1)-rlow(1))/float(crn)  ! x spacing
-	dy = (rhigh(2)-rlow(2))/float(crn)  ! y spacing
+	nx=25
+	ny=nx*2.0/sqrt(3.0)
+	dx = (rhigh(1)-rlow(1))/float(nx)
+	dy = (rhigh(2)-rlow(2))/float(ny)
+	n = nx*ny 
+	!! Isometric grid but still problems...
 	dz = 0.0
 	q = 0   ! counter for regular particles
 	do i=1,n
 c		create some integers in base crn and crn**2
-		j=floor(float(i-1)/float(crn))
-		x = rlow(1)+0.5*dx + dx*mod(i-1,crn)
-		y = rlow(2)+0.5*dy + dy*mod(j,crn)
+		j=floor(float(i-1)/float(nx))
+		x = rlow(1)+0.5*dx + dx*mod(i-1,nx)+mod(j,2)*0.5*dx
+		y = rlow(2)+0.5*dy + dy*mod(j,ny)
 		z = 0d0
 		r(1,i) = x
 		r(2,i) = y
@@ -37,7 +37,7 @@ c		create some integers in base crn and crn**2
 		gm(i) = 1.4d0
 		pc(i) = 0d3
 !		radius = sqrt(r(1,i)**2+r(2,i)**2+r(3,i)**2)
-		if (i.eq.floor(0.5*n))then ! hot in the middle
+		if (i.eq.363)then ! hot in the middle
 		   u(i) = 2.5d0
 		else ! cooler outside
 		   u(i) = 2.5d-5
